f50ab63135049fd8742f7182fb0948e5479e09dd
Ezekiel Templin <zeke@templ.in>
2017-10-19 21:36:22 -0700

    Ruby 2.4 unified Fixnum and Bignum under Integer

--- spec/lita/handler/http_router_spec.rb
+++ spec/lita/handler/http_router_spec.rb
@@ -94,7 +94,7 @@ describe handler, lita_handler: true do
         expect(error).to be_an_instance_of(TypeError)
       end
 
-      expect { http.get("/boom") }.to raise_error(TypeError, "String can't be coerced into Fixnum")
+      expect { http.get("/boom") }.to raise_error(TypeError, "String can't be coerced into Integer")
     end
   end
 end
