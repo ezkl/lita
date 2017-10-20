7ae4e1963dbcd924f896a2e2ce9c717735db6b33
Ezekiel Templin <zeke@templ.in>
2017-10-19 21:43:31 -0700

    Add horrible multi-ruby hack

--- spec/lita/handler/http_router_spec.rb
+++ spec/lita/handler/http_router_spec.rb
@@ -94,7 +94,9 @@ describe handler, lita_handler: true do
         expect(error).to be_an_instance_of(TypeError)
       end
 
-      expect { http.get("/boom") }.to raise_error(TypeError, "String can't be coerced into Integer")
+      number = RbConfig::CONFIG['ruby_version'] =~ /^2\.4.+$/ ? 'Integer' : 'Fixnum'
+
+      expect { http.get("/boom") }.to raise_error(TypeError, "String can't be coerced into #{number}")
     end
   end
 end
